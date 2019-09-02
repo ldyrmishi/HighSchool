aa41ef8beca3dd017e409107de0fb11464dca146
Ledio Dyrmishi <ledio.dyrmishi@ikubinfo.al>
2019-09-02 15:29:08 +0200

    Added SchoolList and Create Views

---
+++ HighSchoolApplication.Web/Controllers/SchoolController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HighSchoolApplication.API.Models;
+using HighSchoolApplication.Web.Factory;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HighSchoolApplication.Web.Controllers
+{
+    public class SchoolController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SchoolModel schoolModel)
+        {
+            if (ModelState.IsValid)
+            {
+                schoolModel.CreatedAt = DateTime.Now;
+                schoolModel.ModifiedAt = DateTime.Now;
+                var data = await HighSchoolApiClientFactory.Instance.AddSchool(schoolModel, HttpContext.Session.GetString("Token"));
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View(schoolModel);
+        }
+    }
+}
