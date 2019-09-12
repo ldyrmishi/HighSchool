97fbcf895a17547bfea6434846620ed548b11e98
Ledio Dyrmishi <ledio.dyrmishi@ikubinfo.al>
2019-09-12 17:36:55 +0200

    Fixes

---
+++ HighSchoolApplication.Web/Models/DigitalRegisterModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HighSchoolApplication.Web.Models
+{
+    public class DigitalRegisterModel
+    {
+        public string FullName { get; set; }
+        public DateTime FirstDayOfWeek { get; set; }
+        public DateTime SecondDayOfWeek { get; set; }
+        public DateTime ThirdDayOfWeek { get; set; }
+        public DateTime FourthDayOfWeek { get; set; }
+        public DateTime FifthDayOfWeek { get; set; }
+    }
+}
