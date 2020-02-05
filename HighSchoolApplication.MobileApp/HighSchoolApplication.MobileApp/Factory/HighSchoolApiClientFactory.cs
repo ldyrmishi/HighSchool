95f6bbe36e64e7f54e1edcc3d902814ce9a8ce92
Ledio <lediodyrmishi0@gmail.com>
2020-02-05 18:34:28 +0100

    Added HighSchool APICLient Factory

---
+++ HighSchoolApplication.MobileApp/HighSchoolApplication.MobileApp/Factory/HighSchoolApiClientFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using HighSchoolApplication.API.Client;
+
+namespace HighSchoolApplication.MobileApp.Factory
+{
+    public static class HighSchoolApiClientFactory
+    {
+        private static Uri apiUri;
+        //private static IConfiguration configuration;
+        private static Lazy<ApiClient> restClient = new Lazy<ApiClient>(() => new ApiClient(apiUri), LazyThreadSafetyMode.ExecutionAndPublication);
+        static HighSchoolApiClientFactory()
+        {
+            apiUri = new Uri("http://localhost:5454/api/");
+        }
+
+        public static ApiClient Instance
+        {
+            get
+            {
+                return restClient.Value;
+            }
+        }
+    }
+}
